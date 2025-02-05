4d4d4848a89a3b005ddd6459aa0572b36b4c3123
Lily Anne Hall <lily.anne.hall@protonmail.com>
2025-02-05 14:43:29 -0800

    higher res banner

--- search.js
+++ search.js
@@ -11,6 +11,7 @@ function filterBy(value) {
   }
 
   value.split(' ').forEach(keyword => {
+    let matched = false;
     keyword = keyword.toLowerCase();
 
     for (let i = 0; i < books.length; i++) {
@@ -21,8 +22,11 @@ function filterBy(value) {
 
       if (title.includes(keyword) || summary.includes(keyword) || tags.includes(keyword)) {
         section.setAttribute('style', '');
+        matched = true;
       } else {
-        section.setAttribute('style', 'display:none');
+        if (!matched) {
+          section.setAttribute('style', 'display:none');
+        }
       }
     }
   });
